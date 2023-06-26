29aac7562db528e636f74b06cdb920630f55c910
Julien Pivotto <roidelapluie@o11y.eu>
2023-06-26 15:28:59 +0200

    Fix CSV import
    Signed-off-by: Julien Pivotto <roidelapluie@o11y.eu>

--- generate-requirements.sh
+++ generate-requirements.sh
@@ -6,7 +6,7 @@ tmpdir=$(mktemp -d)
 python3 -m venv $tmpdir
 source $tmpdir/bin/activate
 pip install --upgrade pip
-pip install ansible
+pip install ansible ara[server] jmespath encodings
 pip freeze > requirements.txt
 
 deactivate
